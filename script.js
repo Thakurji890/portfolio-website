a70fe2e6dc6a9aeb71ddd1573e3e6d78f1487cc4
Thakurji890 <tha.rupesh456@gmail.com>
2026-03-12 22:18:23 +0530

    Add interactive buttons with javaScript

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Portfolio loaded");
+// Smooth scrolling
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// Project button intractive
+
+const buttons = document.querySelectorAll(".project-btn");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    alert("project details coming soon");
+  });
+});
